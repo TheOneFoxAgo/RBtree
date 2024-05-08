465ed21a176ba0bf72fdf8812eafd73896b88b85
TheOneFoxAgo <nikitad2005@gmail.com>
2024-05-08 19:40:53 +0300

    deleted unnessesary methods. Changed tree to real key-value storage. Added check for rbtree

--- BinarySearchTree.h
+++ BinarySearchTree.h
@@ -1,39 +1,60 @@
 #ifndef _BINARY_SEARCH_TREE_H
 #define _BINARY_SEARCH_TREE_H
 
-#include "QueueArray.h"
-#include <iostream>
+#include <functional>
+#include <utility>
 
-template <class T> class BinarySearchTree {
+template < class K, class T, class Compare = std::less< K > >
+class BinarySearchTree
+{
 public:
-  BinarySearchTree() : root_(nullptr) {}
-  BinarySearchTree(const BinarySearchTree<T> &scr) = delete;
-  BinarySearchTree(BinarySearchTree<T> &&src) noexcept : root_(src.root_) {
+  BinarySearchTree():
+    root_(nullptr),
+    compare_({})
+  {}
+  BinarySearchTree(const BinarySearchTree< K, T, Compare >& scr) = delete;
+  BinarySearchTree(BinarySearchTree< K, T, Compare >&& src) noexcept:
+    root_(src.root_)
+  {
     src.root_ = nullptr;
   }
-  BinarySearchTree<T> &operator=(const BinarySearchTree<T> &src) = delete;
-  BinarySearchTree<T> &operator=(BinarySearchTree<T> &&src) noexcept {
+  BinarySearchTree< K, T, Compare >& operator=(const BinarySearchTree< K, T, Compare >& src) = delete;
+  BinarySearchTree< K, T, Compare >& operator=(BinarySearchTree< K, T, Compare >&& src) noexcept
+  {
     std::swap(root_, src.root_);
+    std::swap(compare_, src.compare_);
     return *this;
   }
-  virtual ~BinarySearchTree() {
-    Node *current = root_;
-    if (current) {
-      while (true) {
-        if (current->left_) {
-          current = current->left_;
-        } else if (current->right_) {
-          current = current->right_;
-        } else {
-          Node *next = current->p_;
-          if (!next) {
+  virtual ~BinarySearchTree()
+  {
+    Node* current = root_;
+    if (current)
+    {
+      while (true)
+      {
+        if (current->left)
+        {
+          current = current->left;
+        }
+        else if (current->right)
+        {
+          current = current->right;
+        }
+        else
+        {
+          Node* next = current->p;
+          if (!next)
+          {
             delete current;
             break;
           }
-          if (next->key_ > current->key_) {
-            next->left_ = nullptr;
-          } else {
-            next->right_ = nullptr;
+          if (compare_(current->key, next->key))
+          {
+            next->left = nullptr;
+          }
+          else
+          {
+            next->right = nullptr;
           }
           delete current;
           current = next;
@@ -42,220 +63,228 @@ public:
     }
   }
 
-  bool searchKeyIterative(const T &key) const {
-    Node *candidate = searchNodeIterative(key);
-    return candidate && key == candidate->key_;
+  bool searchKeyIterative(const K& key) const
+  {
+    Node* candidate = searchNodeIterative(key);
+    return candidate && key == candidate->key;
   }
-  bool insertNode(const T &key) {
-    Node *candidate = searchNodeIterative(key);
-    if (!candidate) {
-      root_ = new Node(key);
-    } else if (key == candidate->key_) {
+  bool insertNode(const K& key, const T& value)
+  {
+    Node* candidate = searchNodeIterative(key);
+    if (!candidate)
+    {
+      root_ = new Node{ key, value, Color::Black };
+    }
+    else if (key == candidate->key)
+    {
       return false;
-    } else if (key > candidate->key_) {
-      candidate->right_ = new Node(key, candidate);
-    } else {
-      candidate->left_ = new Node(key, candidate);
+    }
+    else if (compare_(candidate->key, key))
+    {
+      candidate->right = new Node{ key, value, Color::Black, candidate };
+    }
+    else
+    {
+      candidate->left = new Node{ key, value, Color::Black, candidate };
     }
     return true;
   }
-  bool deleteNode(const T &key) {
-    Node *target = searchNodeIterative(key);
-    if (!target || key != target->key_) {
+  bool deleteNode(const K& key)
+  {
+    Node* target = searchNodeIterative(key);
+    if (!target || key != target->key)
+    {
       return false;
     }
-    int nBranches = (target->left_ != nullptr) + (target->right_ != nullptr);
-    switch (nBranches) {
+    int nBranches = (target->left != nullptr) + (target->right != nullptr);
+    switch (nBranches)
+    {
     case 0:
-      updateNodeParent(target, nullptr);
+      updateParentNode(target, nullptr);
       break;
-    case 1: {
-      Node *candidate = target->left_ ? target->left_ : target->right_;
-      candidate->p_ = target->p_;
-      updateNodeParent(target, candidate);
+    case 1:
+    {
+      Node* candidate = target->left ? target->left : target->right;
+      candidate->p = target->p;
+      updateParentNode(target, candidate);
       break;
     }
-    case 2: {
-      Node *candidate = min(target->right_);
-      if (candidate == target->right_) {
-        target->left_->p_ = candidate;
-        candidate->left_ = target->left_;
-        candidate->p_ = target->p_;
-      } else {
-        candidate->p_->left_ = candidate->right_;
-        if (candidate->right_) {
-          candidate->right_->p_ = candidate->p_;
+    case 2:
+    {
+      Node* candidate = minNode(target->right);
+      if (candidate == target->right)
+      {
+        target->left->p = candidate;
+        candidate->left = target->left;
+        candidate->p = target->p;
+      }
+      else
+      {
+        candidate->p->left = candidate->right;
+        if (candidate->right)
+        {
+          candidate->right->p = candidate->p;
         }
 
-        target->right_->p_ = candidate;
-        target->left_->p_ = candidate;
+        target->right->p = candidate;
+        target->left->p = candidate;
 
-        candidate->right_ = target->right_;
-        candidate->left_ = target->left_;
-        candidate->p_ = target->p_;
+        candidate->right = target->right;
+        candidate->left = target->left;
+        candidate->p = target->p;
       }
-      updateNodeParent(target, candidate);
+      updateParentNode(target, candidate);
       break;
     }
     }
     delete target;
     return true;
   }
-  void output(std::ostream &out) const { output(out, root_); }
-  int getNumberOfNodes() const { return getNumberOfNodes(root_); }
-  int getHeight() const { return getHeight(root_) - 1; }
-  void inorderWalkIterative(std::ostream &out) const {
-    Node *current = min(root_);
-    while (current) {
-      out << current->key_ << ' ';
-      current = successor(current);
+  // void inorderWalkIterative(std::ostream &out) const {
+  //   Node *current = min(root_);
+  //   while (current) {
+  //     out << current->key_ << ' ';
+  //     current = successor(current);
+  //   }
+  // }
+  bool isRBTree() const
+  {
+    if (root_->color != Color::Black)
+    {
+      return false;
     }
-  }
-  void inorderWalk(std::ostream &out) const { inorderWalk(root_, out); }
-  void walkByLevels(std::ostream &out) const {
-    if (!root_) {
-      return;
-    }
-    QueueArray<Node *> queue(1 << getHeight());
-    queue.enQueue(root_);
-    while (!queue.isEmpty()) {
-      Node *current = queue.deQueue();
-      out << current->key_ << ' ';
-      if (current->left_) {
-        queue.enQueue(current->left_);
-      }
-      if (current->right_) {
-        queue.enQueue(current->right_);
-      }
-    }
-  }
-  bool isSimilar(const BinarySearchTree<T> &other) const {
-    Node *current = min(root_);
-    Node *otherCurrent = min(other.root_);
-    while (current && otherCurrent) {
-      if (current->key_ == otherCurrent->key_) {
-        current = successor(current);
-        otherCurrent = successor(otherCurrent);
-      } else {
-        return false;
-      }
-    }
-    return !(current || otherCurrent);
-  }
-  bool isIdenticalKey(const BinarySearchTree<T> &other) const {
-    Node *current = min(root_);
-    Node *otherCurrent = min(other.root_);
-    while (current && otherCurrent) {
-      if (current->key_ == otherCurrent->key_) {
-        return true;
-      } else if (current->key_ > otherCurrent->key_) {
-        otherCurrent = successor(otherCurrent);
-      } else {
-        current = successor(current);
-      }
-    }
-    return false;
+    return isWeakRBTree(root_);
   }
 
 private:
-  struct Node {
-    T key_;
-    Node *left_;
-    Node *right_;
-    Node *p_;
-    Node(T key, Node *p = nullptr, Node *left = nullptr, Node *right = nullptr)
-        : key_(key), left_(left), right_(right), p_(p) {}
+  enum class Color
+  {
+    Black,
+    Red
   };
-  Node *searchNodeIterative(const T &key) const {
-    Node *current = root_;
+  struct Node
+  {
+    K key;
+    T value;
+    Color color;
+    Node* p = nullptr;
+    Node* left = nullptr;
+    Node* right = nullptr;
+  };
+  Node* searchNodeIterative(const K& key) const
+  {
+    Node* current = root_;
     bool hasFound = root_ == nullptr;
-    while (!hasFound) {
-      if (key == current->key_) {
+    while (!hasFound)
+    {
+      if (key == current->key)
+      {
         hasFound = true;
-      } else if (key < current->key_) {
-        if (current->left_) {
-          current = current->left_;
-        } else {
+      }
+      else if (compare_(key, current->key))
+      {
+        if (current->left)
+        {
+          current = current->left;
+        }
+        else
+        {
           hasFound = true;
         }
-      } else {
-        if (current->right_) {
-          current = current->right_;
-        } else {
+      }
+      else
+      {
+        if (current->right)
+        {
+          current = current->right;
+        }
+        else
+        {
           hasFound = true;
         }
       }
     }
     return current;
   }
-  void output(std::ostream &out, Node *root) const {
-    out << '(';
-    out << root->key_;
-    if (root->left_) {
-      output(out, root->left_);
-    }
-    if (root->right_) {
-      output(out, root->right_);
-    }
-    out << ')';
-  }
-  int getNumberOfNodes(const Node *node) const {
-    if (node) {
-      return 1 + getNumberOfNodes(node->left_) + getNumberOfNodes(node->right_);
-    } else {
-      return 0;
-    }
-  }
-  int getHeight(const Node *node) const {
-    if (!node) {
-      return 0;
-    } else {
-      int leftHeight = node->left_ ? getHeight(node->left_) : 0;
-      int rightHeight = node->right_ ? getHeight(node->right_) : 0;
-      return std::max(leftHeight, rightHeight) + 1;
-    }
-  }
-  void inorderWalk(Node *node, std::ostream &out) const {
-    if (!node) {
-      return;
-    }
-    inorderWalk(node->left_, out);
-    out << node->key_ << ' ';
-    inorderWalk(node->right_, out);
-  }
-  void updateNodeParent(Node *target, Node *candidate) {
-    if (target->p_) {
-      if (target->p_->key_ > target->key_) {
-        target->p_->left_ = candidate;
-      } else {
-        target->p_->right_ = candidate;
+  void updateParentNode(Node* oldNode, Node* newNode)
+  {
+    if (oldNode->p)
+    {
+      if (compare_(oldNode->key, oldNode->p->key))
+      {
+        oldNode->p->left = newNode;
       }
-    } else {
-      root_ = candidate;
+      else
+      {
+        oldNode->p->right = newNode;
+      }
+    }
+    else
+    {
+      root_ = newNode;
     }
   }
-  Node *successor(Node *node) const {
-    if (node->right_) {
-      return min(node->right_);
-    } else {
-      Node *candidate = node->p_;
-      while (candidate && candidate->right_ == node) {
+  Node* successor(Node* node) const
+  {
+    if (node->right)
+    {
+      return minNode(node->right);
+    }
+    else
+    {
+      Node* candidate = node->p;
+      while (candidate && candidate->right == node)
+      {
         node = candidate;
-        candidate = candidate->p_;
+        candidate = candidate->p;
       }
       return candidate;
     }
   }
-  Node *min(Node *node) const {
-    if (!node) {
+  Node* minNode(Node* node) const
+  {
+    if (!node)
+    {
       return nullptr;
     }
-    while (node->left_) {
-      node = node->left_;
+    while (node->left)
+    {
+      node = node->left;
     }
     return node;
   }
+  // returns black height
+  int isWeakRBTree(const Node* node) const
+  {
+    if (!node)
+    {
+      return 1;
+    }
+    bool isRed = node->color == Color::Red;
+    if (isRed && (colorOf(node->left) == Color::Red || colorOf(node->right) == Color::Red))
+    {
+      return 0;
+    }
+    int result = isWeakRBTree(node->right);
+    if (!result || result != isWeakRBTree(node->left))
+    {
+      return 0;
+    }
+    return result + !isRed;
+  }
+  Color colorOf(const Node* node) const
+  {
+    if (node)
+    {
+      return node->color;
+    }
+    else
+    {
+      return Color::Black;
+    }
+  }
 
-  Node *root_;
+  Node* root_;
+  Compare compare_;
 };
 #endif
--- main.cpp
+++ main.cpp
@@ -1,136 +1,117 @@
-#include "BinarySearchTree.h"
 #include <iostream>
+#include "BinarySearchTree.h"
 
-void test(bool isSuccess, const char *successMessage, const char *failMessage) {
-  if (isSuccess) {
+void test(bool isSuccess, const char* successMessage, const char* failMessage)
+{
+  if (isSuccess)
+  {
     std::cout << successMessage;
-  } else {
+  }
+  else
+  {
     std::cout << failMessage;
   }
 }
-void print(const BinarySearchTree<int> &tree) {
-  tree.output(std::cout);
-  std::cout << '\n';
+template < class T >
+void testRB(const T& tree)
+{
+  test(tree.isRBTree(), "Tree is still RBTree\n", "Tree is not RBTree\n");
 }
-
-void testTree() {
-  BinarySearchTree<int> tree;
-  int nodes[]{10, 5, 13, 4, 7, 11, 14, 6, 8, 12};
-  for (int i : nodes) {
-    tree.insertNode(i);
+void testTree()
+{
+  BinarySearchTree< int, int > tree;
+  int nodes[]{ 10, 5, 13, 4, 7, 11, 14 };
+  for (int i : nodes)
+  {
+    tree.insertNode(i, 0);
   }
-  print(tree);
-  test(tree.insertNode(13), "inserted\n", "not inserted\n");
-  test(tree.insertNode(4), "inserted\n", "not inserted\n");
-
-  print(tree);
-
-  std::cout << "count: " << tree.getNumberOfNodes() << '\n';
-  std::cout << "height: " << tree.getHeight() << '\n';
-
+  testRB(tree);
+  test(tree.insertNode(13, 0), "inserted\n", "not inserted\n");
+  test(tree.insertNode(4, 0), "inserted\n", "not inserted\n");
+  testRB(tree);
   test(tree.searchKeyIterative(100), "found\n", "not found\n");
   test(tree.searchKeyIterative(4), "found\n", "not found\n");
 
-  int nodesToDelete[]{5, 11, 14, 15};
-  for (int i : nodesToDelete) {
+  int nodesToDelete[]{ 5, 11, 14, 15 };
+  for (int i : nodesToDelete)
+  {
     std::cout << "Trying to delete " << i << ": ";
     test(tree.deleteNode(i), "deleted\n", "not deleted\n");
-    print(tree);
-    std::cout << "count: " << tree.getNumberOfNodes() << '\n';
   }
-  tree.insertNode(14);
+  testRB(tree);
+  tree.insertNode(14, 0);
+  testRB(tree);
 
-  BinarySearchTree<int> moved(std::move(tree));
+  BinarySearchTree< int, int > moved(std::move(tree));
 }
 
-void testDestructorAfterDelete() {
-  BinarySearchTree<int> tree;
+void testDestructorAfterDelete()
+{
+  BinarySearchTree< int, int > tree;
   int nodes[]{
-      22376, 19148, 9064,  4536,  26826, 7510,  16348, 18792, 23507, 6724,
-      12960, 11559, 9879,  11023, 18260, 30890, 3200,  2071,  28381, 32676,
-      17456, 448,   12726, 13838, 9644,  24207, 2139,  3207,  13422, 2533,
-      7402,  10504, 2445,  1443,  2830,  20904, 20377, 2771,  9468,  8436,
-      4157,  16521, 13710, 17472, 14788, 12956, 4630,  4888,  1934,  4186,
-      26033, 25914, 28231, 27376, 20052, 4866,  28271, 22225, 1220,  32728,
-      7044,  12356, 17215, 5488,  6378,  30255, 4676,  7822,  15889, 6895,
-      21118, 24465, 20807, 2358,  4513,  31062, 25879, 8793,  13356, 30480,
-      413,   14781, 9249,  8973,  29417, 28937, 17829, 10060, 8700,  2880,
-      27160, 5242,  11925, 9964,  21235, 15457, 11831, 4317,  23535, 9510,
+    22376, 19148, 9064,  4536,  26826, 7510,  16348, 18792, 23507, 6724,  12960, 11559, 9879,  11023, 18260,
+    30890, 3200,  2071,  28381, 32676, 17456, 448,   12726, 13838, 9644,  24207, 2139,  3207,  13422, 2533,
+    7402,  10504, 2445,  1443,  2830,  20904, 20377, 2771,  9468,  8436,  4157,  16521, 13710, 17472, 14788,
+    12956, 4630,  4888,  1934,  4186,  26033, 25914, 28231, 27376, 20052, 4866,  28271, 22225, 1220,  32728,
+    7044,  12356, 17215, 5488,  6378,  30255, 4676,  7822,  15889, 6895,  21118, 24465, 20807, 2358,  4513,
+    31062, 25879, 8793,  13356, 30480, 413,   14781, 9249,  8973,  29417, 28937, 17829, 10060, 8700,  2880,
+    27160, 5242,  11925, 9964,  21235, 15457, 11831, 4317,  23535, 9510,
   };
-  for (int i : nodes) {
-    tree.insertNode(i);
+  for (int i : nodes)
+  {
+    tree.insertNode(i, 0);
   }
+  testRB(tree);
   std::cout << "Deleting nodes\n";
-  int nodesToDelete[]{12726, 2830, 1220};
-  for (int i : nodesToDelete) {
+  int nodesToDelete[]{ 12726, 2830, 1220 };
+  for (int i : nodesToDelete)
+  {
     tree.deleteNode(i);
   }
+  testRB(tree);
   std::cout << "Testing destructor. If not segfaulted, test is successful.\n";
 }
 
-void testWalk() {
-  BinarySearchTree<int> tree;
+void testWalk()
+{
+  BinarySearchTree< int, int > tree;
   int nodes[]{
-      23, 16, 12, 19, 17, 24, 5,  1, 4,  7, 11, 6,  22, 20, 9,
-      27, 10, 2,  13, 15, 25, 18, 3, 14, 8, 29, 26, 30, 21, 28,
+    23, 16, 12, 19, 17, 24, 5, 1, 4, 7, 11, 6, 22, 20, 9, 27, 10, 2, 13, 15, 25, 18, 3, 14, 8, 29, 26, 30, 21, 28,
   };
-  for (int i : nodes) {
-    tree.insertNode(i);
+  for (int i : nodes)
+  {
+    tree.insertNode(i, 0);
   }
-  std::cout << "Testing infix walking\n";
-  tree.inorderWalk(std::cout);
-  std::cout << '\n';
-  tree.inorderWalkIterative(std::cout);
-  std::cout << '\n';
-  BinarySearchTree<int> secondTree;
-  for (int i : nodes) {
-    secondTree.insertNode(((i + 6) % 30) + 1); // shuffling nodes.
+  testRB(tree);
+  BinarySearchTree< int, int > secondTree;
+  for (int i : nodes)
+  {
+    secondTree.insertNode(((i + 6) % 30) + 1, 0); // shuffling nodes.
   }
-  std::cout << "Are two equal trees equal??? "
-            << ((tree.isSimilar(secondTree)) ? "yes" : "no") << '\n';
-  secondTree.insertNode(0);
-  std::cout << "Are two unequal trees equal??? "
-            << ((tree.isSimilar(secondTree)) ? "yes" : "no") << '\n';
+  testRB(secondTree);
+  // std::cout << "Are two equal trees equal??? "
+  //           << ((tree.isSimilar(secondTree)) ? "yes" : "no") << '\n';
+  // secondTree.insertNode(0);
+  // std::cout << "Are two unequal trees equal??? "
+  //           << ((tree.isSimilar(secondTree)) ? "yes" : "no") << '\n';
 
-  BinarySearchTree<int> thirdTree;
-  int otherNodes[]{35, 33, 39, 32, 38, 31, 37, 40, 34, 36, 0};
-  for (int i : otherNodes) {
-    thirdTree.insertNode(i);
+  BinarySearchTree< int, int > thirdTree;
+  int otherNodes[]{ 35, 33, 39, 32, 38, 31, 37, 40, 34, 36, 0 };
+  for (int i : otherNodes)
+  {
+    thirdTree.insertNode(i, 0);
   };
-  std::cout << "Are there any similar keys, when there are none?? "
-            << ((tree.isIdenticalKey(thirdTree)) ? "yes" : "no") << '\n';
-  std::cout << "Are there any similar keys, when there are one?? "
-            << ((secondTree.isIdenticalKey(thirdTree)) ? "yes" : "no") << '\n';
-  BinarySearchTree<int> forthTree;
-  BinarySearchTree<int> fifthTree;
-  std::cout << "Are two empty trees equal??? "
-            << ((forthTree.isSimilar(fifthTree)) ? "yes" : "no") << '\n';
-  std::cout << "Are there any similar keys, when both trees are empty?? "
-            << ((forthTree.isIdenticalKey(fifthTree)) ? "yes" : "no") << '\n';
+  testRB(thirdTree);
+  BinarySearchTree< int, int > forthTree;
+  BinarySearchTree< int, int > fifthTree;
+  // std::cout << "Are two empty trees equal??? "
+  //           << ((forthTree.isSimilar(fifthTree)) ? "yes" : "no") << '\n';
 }
-void testWidthWalk() {
-  BinarySearchTree<int> tree;
-  int nodes[]{29, 10, 14, 7,  18, 22, 24, 5,  6,  23, 17, 25, 15, 9,  1, 12,
-              2,  31, 11, 30, 4,  19, 3,  28, 26, 20, 13, 8,  21, 16, 27};
-  for (int i : nodes) {
-    tree.insertNode(i);
-  };
-  std::cout << "Tree looks like this: \n";
-  tree.output(std::cout);
-  std::cout << "\nInfix walk:\n";
-  tree.inorderWalk(std::cout);
-  std::cout << "\nWidth walk:\n";
-  tree.walkByLevels(std::cout);
-  std::cout << '\n';
-  tree.insertNode(0);
-  tree.walkByLevels(std::cout);
-  std::cout << '\n';
-}
-int main() {
+int main()
+{
   testTree();
   testDestructorAfterDelete();
   testWalk();
-  testWidthWalk();
   std::cout << "Success\n";
   return 0;
 }
